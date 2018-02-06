13f557f6692c08ffbaa7e3a24db18398a4ce6fba
Sam Singer <samuel.singer1@gmail.com>
2018-02-06 16:15:19 -0700

    Seeds & Migrations Set-up

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+// const database = {
+//   "companies": [
+//     { "id": 1,
+//     "name": "MIC" },
+//     { "id": 2,
+//     "name": "SED" },
+//     { "id": 3,
+//     "name": "TUT" },
+//     { "id": 4,
+//     "name": "ORT" },
+//   ]
+// };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +26,16 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
   response.send("Hello!")
+});
+
+app.get('/api/v1/companies', (request, response) => {
+  database("companies").select()
+    .then((companies) => {
+      response.status(200).json(companies);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
