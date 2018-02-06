4eeb4bf3f95c01504b40b5f05d28252b26b50697
Pat Neel <patrick.neel@colorado.edu>
2018-02-05 19:46:26 -0700

    added server file, and knex file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log(`app is running on ${app.get('port')}.`)
+})
