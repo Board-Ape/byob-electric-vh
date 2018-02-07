34e19d4a8c5eaa8dbd9c6fd1dadadde540a4c709
Pat Neel <patrick.neel@colorado.edu>
2018-02-06 18:43:14 -0700

    added json files

--- db/migrations/20180206160440_initial.js
+++ db/migrations/20180206160440_initial.js
@@ -2,28 +2,31 @@ exports.up = (knex, Promise) => {
   return Promise.all([
     knex.schema.createTable('topcompanies', (table) => {
       table.increments('id').primary();
-      table.string('name');
+      table.string('companyName');
+      table.string('industry');
+      table.string('location');
+      table.string('revenueGrowth');
 
       table.timestamps(true, true);
-    }), // no semi-colon here sinse it's an array
+    }), 
 
     knex.schema.createTable('branches', (table) => {
       table.increments('id').primary();
-      table.string('branch');
-      // creating a foreign key
+      table.string('branchName');
+      table.string('employees');
+      table.string('location');
+      table.string('grossRevenue');
       table.integer('company_id').unsigned();
       table.foreign('company_id').references('topcompanies.id');
 
       table.timestamps(true, true);
     }),
 
-  ]); // end Promise.all
+  ]);
 };
 
 exports.down = (knex, Promise) => {
   return Promise.all([
-    // be aware of how you are deleting schemas
-    // footnotes relies on papers so should come before
     knex.schema.dropTable('branches'),
     knex.schema.dropTable('topcompanies')
   ]);
