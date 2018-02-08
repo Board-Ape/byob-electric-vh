764c4b9459798c966b818b6019fb7a6ce35e0b21
Sam Singer <samuel.singer1@gmail.com>
2018-02-08 13:52:05 -0700

    Update JSON File

--- db/seeds/test/test_data.js
+++ db/seeds/test/test_data.js
@@ -1,6 +1,6 @@
 
 exports.seed = function(knex, Promise) {
-  
+
   return knex('branches').del()
     .then(() => knex('topcompanies').del())
 
@@ -8,32 +8,32 @@ exports.seed = function(knex, Promise) {
 
       return Promise.all([
         knex('topcompanies').insert(
-        {
-          companyName: 'API Industries',
-          industry: 'machinery',
-          location: 'New South Wales',
-          revenueGrowth: '200%'
-        }, 'id')
-        .then(branches => {
-          return knex('branches').insert([
-            {
-              companyName: 'API Industries', 
-              employees: '1200', 
-              branchName: 'New South', 
-              grossRevenue: '1,200,000',
-              company_id: branches[0]
-            },
-            {
-              companyName: 'API Industries', 
-              employees: '20000', 
-              branchName: 'Circle Group', 
-              grossRevenue: '1200000000',
-              company_id: branches[0]
-            }
-          ])
-        })
-        .then(() => console.log('seeding complete'))
-        .catch(error => console.log(`Error seeding data: ${error}`))
+          {
+            companyName: 'API Industries',
+            industry: 'machinery',
+            location: 'New South Wales',
+            revenueGrowth: '200%'
+          }, 'id')
+          .then(branches => {
+            return knex('branches').insert([
+              {
+                companyName: 'API Industries',
+                employees: '1200',
+                branchName: 'New South',
+                grossRevenue: '1,200,000',
+                company_id: branches[0]
+              },
+              {
+                companyName: 'API Industries',
+                employees: '20000',
+                branchName: 'Circle Group',
+                grossRevenue: '1200000000',
+                company_id: branches[0]
+              }
+            ]);
+          })
+          .then(() => console.log('seeding complete'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
       ]) //end Promise.all
     });
 };
