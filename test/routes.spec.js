3b6a1ee327cf9637a1f58ace7884cc992c1c0c26
Pat Neel <patrick.neel@colorado.edu>
2018-02-08 12:14:06 -0700

    added get tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,3 +7,47 @@ const server = require('../server');
 const knex = require('../db/knex.js')
 
 chai.use(chaiHttp);
+
+describe('Client Side Routes', () => {
+  it.skip('Should return the home page', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200)
+      response.should.be.html
+    })
+    .catch(error => {
+      return error
+    })
+  })
+})
+
+describe('API Routes', () => {
+  it('should get all of the companies', () => {
+    return chai.request(server)
+    .get('/api/v1/companies')
+    .then(response => {
+      response.status.have.status(200)
+      response.should.be.json
+      response.body.should.be.a('object')
+      response.res.should.be.a('object')
+    })
+    .catch(error => {
+      return error;
+    })
+  })
+
+  it('should get all the branches', () => {
+    return chai.request(server)
+    .get('/api/v1/branches')
+    .then(response => {
+      response.status.have.status(200)
+      response.should.be.json
+      response.body.should.be.a('object')
+      response.res.should.be.a('object')
+    })
+    .catch(error => {
+      return error;
+    })
+  })
+})
